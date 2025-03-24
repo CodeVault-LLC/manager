6b3ab1e45e10f9948c4b6404f2d719067aa56317
Lukas Moe Olsen <lukas@dyplink.no>
2025-03-24 12:57:31 +0100

    bugfix ipc client

--- app/src/renderer/src/utils/ipcClient.ts
+++ app/src/renderer/src/utils/ipcClient.ts
@@ -1,4 +1,6 @@
 import { IpcHandlers } from '@shared/types/ipc'
+import { store } from './store-context'
+import { EErrorCodes } from '@shared/helpers'
 
 // Typed wrapper for IPC communication
 export const ipcClient = {
@@ -6,6 +8,10 @@ export const ipcClient = {
     channel: T,
     ...args: Parameters<IpcHandlers[T]>
   ): Promise<Awaited<ReturnType<IpcHandlers[T]>>> => {
+    if (store.error.getError(EErrorCodes.NETWORK_ERROR)) {
+      return Promise.reject(new Error('Network error')) as Promise<Awaited<ReturnType<IpcHandlers[T]>>>
+    }
+
     return window.electron.ipcRenderer.invoke(channel, ...args)
   }
 }
